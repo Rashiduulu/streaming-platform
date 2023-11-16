bbcb9e43a670671f7f492a1b7c000446bb1d06a0
Nurtilek Rashid uulu <98835365+Rashiduulu@users.noreply.github.com>
2023-11-16 15:17:04 +0900

    Update firebase-config.ts

--- netflix-ui/src/untils/firebase-config.ts
+++ netflix-ui/src/untils/firebase-config.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyArMYtGMx5JfhTzDMh3Aqe8lppLPtrwEe8",
-    authDomain: "react-netflix-clone-a032a.firebaseapp.com",
-    projectId: "react-netflix-clone-a032a",
-    storageBucket: "react-netflix-clone-a032a.appspot.com",
-    messagingSenderId: "566307160365",
-    appId: "1:566307160365:web:a309046c4c818b2c1453a4",
-    measurementId: "G-6FMWVBLPFX"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
